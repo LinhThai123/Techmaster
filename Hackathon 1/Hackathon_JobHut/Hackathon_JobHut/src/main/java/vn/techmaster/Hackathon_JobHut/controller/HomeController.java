a6fe9a668fb971bb824a89f06de41ac5d57b0480
Linhthai123 <Linhthai130100@gmail.com>
2022-05-09 16:15:50 +0700

    search , detail job

--- Hackathon 1/Hackathon_JobHut/Hackathon_JobHut/src/main/java/vn/techmaster/Hackathon_JobHut/controller/HomeController.java
+++ Hackathon 1/Hackathon_JobHut/Hackathon_JobHut/src/main/java/vn/techmaster/Hackathon_JobHut/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class HomeController {
+public class  HomeController {
     public String showHomePage () {
         return "index" ;
     }
